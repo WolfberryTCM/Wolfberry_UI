17c5e89cffd4028e228bc86cc14b6deb00523f44
TargaryenQi <44879154+TargaryenQi@users.noreply.github.com>
2019-11-18 15:29:18 -0500

    the innerbook could see the service list now

--- src/components/innerBook/innerbookbook/innerBookService.component.jsx
+++ src/components/innerBook/innerbookbook/innerBookService.component.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const innerBookService = () => {
-  return <div>Service List</div>;
+const innerBookService = props => {
+  const { profile } = props.location;
+  const { services } = profile;
+  return (
+    <ul>
+      {services.map((service, index) => (
+        <li key={index}>{service.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default innerBookService;
